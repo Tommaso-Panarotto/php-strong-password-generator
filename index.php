4359756a544ae4a47f1ddd46d0c04bb05531f3f7
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-07 15:00:00 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password generator</title>
+</head>
+
+<body>
+    <h1><?= "PHP"; ?></h1>
+</body>
+
+</html>
